c2f85534a17b7fca948c921946b66eb265ffc8c2
Luca Veca <lucaveca@gmail.com>
2015-04-14 11:27:10 -0700

    Milestone 4

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+//calls the database
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+//$mysqli->connect_error then we want it to die and have this message
+if ($mysqli->connect_error) {
+	die('Connect Error (' . mysqli_>connect_errno . ')'
+		. $mysqli->connect_error);
+}
+else{
+	echo "Connection made";
+}
+mysqli->close();
+?>
