ddb31b9ac54528173e65ea108a5322e8c72cb273
Luca Veca <lucaveca@gmail.com>
2015-04-16 10:56:05 -0700

    Milestone 9

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 <head>
 	<title> Simple To-Do List </title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 </head>
 <body>
+	<div class="title">
+		<h1>To-Do<p>List</p></h1>
+	</div>
 	<div class="wrap">
 		<div class="task-list">
 			<ul>
@@ -16,4 +20,23 @@
 	</form>
 	</div>	
 </body>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task();//calling the add task function
+
+	function add_task(){
+			$('.add-new-task').submit(function(){
+				var new_task = $('.add-new-task input[name=new-task').val();
+
+				if (new_task != '') {
+					$.post('includes/add-task.php', { task: new_task}, function(data){
+						$(('add-new-task input[name=new-task]').val();
+							$(data).appendTo('task-list ul').hide().fadeIn();
+					)
+					});
+				}
+				return false;
+			});
+	}
+</script>
 </html>
