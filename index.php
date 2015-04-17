b6306688c50aec133ced061e8758be03cf3e3a2e
Luca Veca <lucaveca@gmail.com>
2015-04-17 10:47:49 -0700

    Milestone 14

--- index.php
+++ index.php
@@ -37,5 +37,17 @@
 				return false;
 			});
 	}
+
+	$('.delete-button').click(function(){
+			var current_element = $(this);
+			var task_id = $(this).attr('id');
+			//calls delete-task.php
+			$.post('includes/delete-task.php', {id: task_id}, function(){
+				//calls variables
+				current_element.parent().fadeOut("fast", function(){
+					$(this).remove();
+				});
+			});
+		});
 </script>
 </html>
